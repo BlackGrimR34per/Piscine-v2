89679b104ac6792fdd2067358e530568bd0de295
Yohen Sheraun <ysheraun20@yahoo.com>
2025-04-08 00:26:01 +0800

    Up to ex07

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 00:17:35 by yosherau          #+#    #+#             */
+/*   Updated: 2025/04/08 00:25:47 by yosherau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	start;
+	int	end;
+
+	start = 0;
+	end = size - 1;
+	while (start < end)
+		ft_swap(&tab[start++], &tab[end--]);
+}
